c392aa7c365218bed45e05158d723a38ad263eb7
Naeim <naeim.smz@gmail.com>
2025-06-08 17:58:23 +0430

    create browser router and nest routes in to them

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./features/ui/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
+
 function App() {
-  return <div>Hello vite!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
