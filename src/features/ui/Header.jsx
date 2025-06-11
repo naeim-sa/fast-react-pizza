3a5478beb4f8bcdcc3b123078d54f13fafeff6b5
Naeim <naeim.smz@gmail.com>
2025-06-11 19:44:20 +0430

    responsive design

--- src/features/ui/Header.jsx
+++ src/features/ui/Header.jsx
@@ -5,7 +5,7 @@ import UserName from '../user/UserName';
 
 function Header() {
   return (
-    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
