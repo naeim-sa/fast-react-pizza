81e2446a96a6da232246dee25b906a58b0b73eb0
Naeim <naeim.smz@gmail.com>
2025-06-11 15:08:44 +0430

    box model spacing borders and display styling with tailwind

--- src/features/ui/Header.jsx
+++ src/features/ui/Header.jsx
@@ -5,7 +5,7 @@ import UserName from '../user/UserName';
 
 function Header() {
   return (
-    <header className="bg-yellow-500 uppercase">
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,5 @@
 function UserName() {
-  return <div className="text-sm font-semibold">Naeim</div>;
+  return <div className="hidden text-sm font-semibold">Naeim</div>;
 }
 
 export default UserName;
