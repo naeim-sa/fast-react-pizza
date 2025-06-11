e7517ab287ff4069cc6dd8c190acf576c95e3593
Naeim <naeim.smz@gmail.com>
2025-06-11 14:18:12 +0430

    styling text and creating usename compo

--- src/features/ui/Header.jsx
+++ src/features/ui/Header.jsx
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom';
+
 import SearchOrder from '../order/SearchOrder';
+import UserName from '../user/UserName';
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>Naeim</p>
+      <UserName />
     </header>
   );
 }
---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,5 @@
+function UserName() {
+  return <div className="text-sm font-semibold">Naeim</div>;
+}
+
+export default UserName;
