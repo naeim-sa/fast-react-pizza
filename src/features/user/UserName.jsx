d7d0cae195c3acf947f75da7dc14d0e06a99174a
Naeim <naeim.smz@gmail.com>
2025-06-11 22:11:29 +0430

    using flex for some compo styling

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,5 @@
 function UserName() {
-  return <div className="hidden text-sm font-semibold">Naeim</div>;
+  return <div className="hidden text-sm font-semibold md:block">Naeim</div>;
 }
 
 export default UserName;
