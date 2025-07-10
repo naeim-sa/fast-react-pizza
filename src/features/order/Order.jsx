b72d884b64a09a80cee96522667ee81975729902
Naeim <naeim.smz@gmail.com>
2025-07-10 21:09:02 +0430

    updating data without navigation

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -10,6 +10,7 @@ import {
   formatDate,
 } from '../../utils/helpers';
 import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   const order = useLoaderData();
@@ -93,6 +94,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
